5e8feb042982acb33764e3cdc5537440d4708a03
Zain Asif <zainasif7345@gmail.com>
2024-07-16 22:11:56 +0500

    Fixed UI of calender

--- src/components/ui/AddPaymentCard.tsx
+++ src/components/ui/AddPaymentCard.tsx
@@ -36,7 +36,7 @@ export const AddPaymentCard = ({ docId, fetchUserDocs }) => {
         className="bg-slate-700 hover:bg-slate-600 max-sm:w-11/12 max-md:w-5/12 max-lg:w-2/6 max-xl:w-2/6 max-2xl:w-3/12 2xl:w-3/12 flex flex-col border-2 border-black rounded p-4 gap-2"
       >
         <h1 className="text-xl uppercase font-extrabold mx-auto my-auto">
-          add payment
+          Add payment
         </h1>
       </button>
       {showModal ? (
@@ -85,7 +85,7 @@ export const AddPaymentCard = ({ docId, fetchUserDocs }) => {
                     <textarea
                       id="description"
                       className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="Once upon a time in a descriptive payment land"
+                      placeholder="Description..."
                       ref={descRef}
                       onChange={(e) => (descRef.current.value = e.target.value)}
                       required
@@ -99,6 +99,21 @@ export const AddPaymentCard = ({ docId, fetchUserDocs }) => {
                       Due Date
                     </label>
                     <div className="relative max-w-sm">
+                      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg
+                          aria-hidden="true"
+                          className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                            clipRule="evenodd"
+                          ></path>
+                        </svg>
+                      </div>
                       <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
                       <input
                         type="date"
