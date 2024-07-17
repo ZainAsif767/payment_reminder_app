163ecc315bc3563077dab81e13d0ad132779f44a
Zain Asif <zainasif7345@gmail.com>
2024-07-17 15:50:50 +0500

    minor Fixes

--- src/components/ui/AddPaymentCard.tsx
+++ src/components/ui/AddPaymentCard.tsx
@@ -98,7 +98,7 @@ export const AddPaymentCard = ({ docId, fetchUserDocs }) => {
                       type="title"
                       id="title"
                       className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="My First Payment"
+                      placeholder="Title for payment"
                       required
                       ref={titleRef}
                     />
@@ -163,9 +163,9 @@ export const AddPaymentCard = ({ docId, fetchUserDocs }) => {
                   </div>
                 </div>
                 {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                <div className="flex items-center justify-center p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="bg-red-500 text-white active:bg-red-600 font-bold  uppercase px-6 py-3 rounded shadow text-sm hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
