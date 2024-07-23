64a672a239136c53c4f438b8942bca2168d77506
Zain Asif <zainasif7345@gmail.com>
2024-07-23 22:36:11 +0500

    react-push-notification testing

--- src/components/ui/Dashboard.tsx
+++ src/components/ui/Dashboard.tsx
@@ -18,6 +18,7 @@ import { AddPaymentCard } from "./AddPaymentCard";
 import { getToken, onMessage } from "firebase/messaging";
 import { MySwal, toast } from "../utils/swal";
 import { Box, Button, Modal, Typography } from "@mui/material";
+import addNotification from "react-push-notification";
 
 async function requestPermission(docId) {
   const permission = await Notification.requestPermission();
@@ -80,6 +81,17 @@ export default function Dashboard() {
     if (error) console.error(error.message);
     fetchUserName();
   }, [user, loading, error]);
+
+  // to send push notification from browser
+  // const buttonClick = () => {
+  //   addNotification({
+  //     title: "Warning",
+  //     subtitle: "This is a subtitle",
+  //     message: "This is a very long message",
+  //     theme: "darkblue",
+  //     native: true, // when using native, your OS will handle theming.
+  //   });
+  // };
 
   const fetchUserName = async () => {
     try {
@@ -183,13 +195,19 @@ export default function Dashboard() {
                 />
               ))}
             </div>
-            <div className="flex justify-center items-center mt-4">
+            <div className="flex justify-center items-center mt-4 gap-5">
               <button
                 className="bg-sky-500 p-4 text-white rounded-md"
                 onClick={() => setModalOpen(true)}
               >
                 Due Payments
               </button>
+              {/* <button
+                className="bg-red-400 p-4 text-white rounded-md"
+                onClick={buttonClick}
+              >
+                Notify
+              </button> */}
             </div>
             <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
               <Box
